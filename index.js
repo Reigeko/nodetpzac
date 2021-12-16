1221c111f549a8b5a94676083fe2ef79a4b3ce55
Serra Zacharie <zacharie.serra@etud.fr>
2021-12-16 11:01:05 +0100

    okok

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+var request = require("request");
 const PORT = process.env.PORT || 5000 // this is very important
 
+
+var options = { method: 'GET',
+  url: 'https://nodetpbd-4f62.restdb.io/rest/account',
+  headers:
+   { 'cache-control': 'no-cache',
+     'x-apikey': '1f974012daf1e196860fa4fab287595516513' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!' + request()),
 })
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
+// dsfsdf
