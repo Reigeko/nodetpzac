342d62e56a5e4aa7ba74cb61295daf56caff1d5b
Serra Zacharie <zacharie.serra@etud.fr>
2021-12-16 12:09:48 +0100

    dfnon

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const app = express()
-const request = require("request");
+// const request = require("request");
 const PORT = process.env.PORT || 5000 // this is very important
 
 const axios = require('axios').create({
-  url: 'https://nodetpbd-4f62.restdb.io/rest/account',
+  url: 'https://nodetpbd-4f62.restdb.io/rest',
   headers:
    { 'cache-control': 'no-cache',
      'x-apikey': '1f974012daf1e196860fa4fab287595516513' }
-     });
+   });
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
 
-  console.log(axios);
-});
+app.get('/', async function (req, res) {
+  res.send(await getAccount())
+})
 
-app.get('/', function (req, res) {
-  res.send('Hello World!'),
+async function getAccount() {
+  return await axios.get('/account').then((resultat)=>{return resultat.data})
+}
+
+app.get('/', async function (req, res) {
+  res.send('Hello World!')
 })
 
 app.listen(PORT, function () {
